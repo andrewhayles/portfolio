29d16a1af9eff647ca4b6b2ffc71e8f8c9a286e8
Andrew Hayles <andyhayles@gmail.com>
2025-07-20 16:06:29 +0800

    troubleshooting

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,5 +1,4 @@
 import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
 import { allModels } from './.stackbit/models';
 
 const config = defineStackbitConfig({
