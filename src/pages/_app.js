613700ac1c862066b1d580543ac53e414f8123c1
Andrew Hayles <andyhayles@gmail.com>
2025-09-12 19:12:38 +0800

    javascript and css updates to improve performance

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,25 @@
+// pages/_app.js
+
 import { generateGlobalCssVariables } from '@/utils/theme-style-utils';
 import { useEffect, useState } from 'react';
 import '../css/main.css';
+import { DM_Mono, Azeret_Mono } from 'next/font/google';
+
+// 1. CONFIGURE THE FONTS HERE
+const dmMono = DM_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  style: ['normal', 'italic'],
+  variable: '--font-dm-mono', // Creates a CSS variable
+});
+
+const azeretMono = Azeret_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  style: ['normal', 'italic'],
+  variable: '--font-azeret-mono', // Creates another CSS variable
+});
+
 
 export default function MyApp({ Component, pageProps }) {
     const { global, ...page } = pageProps;
@@ -15,13 +34,14 @@ export default function MyApp({ Component, pageProps }) {
     }, [page.colors]);
 
     return (
-        <>
+        // 2. APPLY THE FONT VARIABLES TO A WRAPPING ELEMENT
+        <div className={`${dmMono.variable} ${azeretMono.variable}`}>
             <style jsx global>{`
                 :root {
                     ${cssVars}
                 }
             `}</style>
             {isMounted ? <Component {...pageProps} /> : null}
-        </>
+        </div>
     );
 }
