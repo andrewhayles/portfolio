18740d6f52d0ee61aaa9057c83d31ecf202112c2
Andrew Hayles <andyhayles@gmail.com>
2025-09-13 17:24:02 +0800

    improve performance

--- src/pages/api/code/[slug].js
+++ src/pages/api/code/[slug].js
@@ -1,13 +1,17 @@
-import { getPage } from '@/utils/get-page'; // Adjust import to your data-fetching util
+import { allContent } from '@/utils/content';
 
-export default async function handler(req, res) {
-    const slug = req.query.slug;
-    const page = await getPage(slug); // This function must get the content for one page
-    
+export default function handler(req, res) {
+    const { slug } = req.query;
+    const allData = allContent();
+
+    // Find the project where the end of its URL path matches the slug
+    const page = allData.find((p) => p.__metadata.urlPath.endsWith(`/${slug}`));
+
+    // If the page or its code doesn't exist, return an error
     if (!page || !page.code) {
         return res.status(404).json({ error: 'Code not found' });
     }
-    
-    // Return only the code
+
+    // Otherwise, return just the code
     res.status(200).json({ code: page.code });
 }
