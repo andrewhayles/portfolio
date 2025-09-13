e117bb71ec6e05eab31aa1e162dbbadb77034631
Andrew Hayles <andyhayles@gmail.com>
2025-09-13 13:26:23 +0800

    improvements to performance 1

--- src/components/ViewportAware.tsx
+++ src/components/ViewportAware.tsx
@@ -1,21 +1,20 @@
-// components/ViewportAware.js
 import { useInView } from 'react-intersection-observer';
+import { ReactNode } from 'react';
 
-// You can adjust the rootMargin to control how early or late the component loads.
-// '200px' means it will start loading when it's 200px away from the viewport.
 const options = {
   triggerOnce: true,
   rootMargin: '200px 0px',
 };
 
-export const ViewportAware = ({ children }) => {
+interface Props {
+  children: ReactNode;
+}
+
+export const ViewportAware = ({ children }: Props) => {
   const { ref, inView } = useInView(options);
 
   return (
     <div ref={ref}>
-      {/* This is the key part: The children (your DynamicComponent) are only
-        rendered when 'inView' becomes true. Until then, it renders nothing.
-      */}
       {inView ? children : null}
     </div>
   );
