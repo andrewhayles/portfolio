cb354dbb65a558339f3dd517ed000ccd5b000929
Andrew Hayles <andyhayles@gmail.com>
2025-09-18 21:38:13 +0800

    lazy on load

--- src/components/components-registry.tsx
+++ src/components/components-registry.tsx
@@ -13,28 +13,15 @@ const dyn = (importer: () => Promise<any>) =>
   dynamic(importer, { ssr: false, loading: () => null });
 
 const dynamicComponents = {
-  CheckboxFormControl: dyn(() => import('./molecules/FormBlock/CheckboxFormControl')),
   ContactSection: dyn(() => import('./sections/ContactSection')),
   CtaSection: dyn(() => import('./sections/CtaSection')),
   DividerSection: dyn(() => import('./sections/DividerSection')),
   EmailFormControl: dyn(() => import('./molecules/FormBlock/EmailFormControl')),
-  FeaturedItem: dyn(() => import('./sections/FeaturedItemsSection/FeaturedItem')),
-  FeaturedItemsSection: dyn(() => import('./sections/FeaturedItemsSection')),
-  FeaturedPostsSection: dyn(() => import('./sections/FeaturedPostsSection')),
   FeaturedProjectsSection: dyn(() => import('./sections/FeaturedProjectsSection')),
   FormBlock: dyn(() => import('./molecules/FormBlock')),
   ImageBlock: dyn(() => import('./molecules/ImageBlock')),
-  MediaGallerySection: dyn(() => import('./sections/MediaGallerySection')),
-  PostFeedSection: dyn(() => import('./sections/PostFeedSection')),
   ProjectFeedSection: dyn(() => import('./sections/ProjectFeedSection')),
-  RecentPostsSection: dyn(() => import('./sections/RecentPostsSection')),
-  RecentProjectsSection: dyn(() => import('./sections/RecentProjectsSection')),
-  QuoteSection: dyn(() => import('./sections/QuoteSection')),
-  SelectFormControl: dyn(() => import('./molecules/FormBlock/SelectFormControl')),
   LabelsSection: dyn(() => import('./sections/LabelsSection')),
-  // TestimonialsSection removed because you don't use it.
-  TextareaFormControl: dyn(() => import('./molecules/FormBlock/TextareaFormControl')),
-  TextFormControl: dyn(() => import('./molecules/FormBlock/TextFormControl')),
   TextSection: dyn(() => import('./sections/TextSection')),
   PageLayout: dyn(() => import('./layouts/PageLayout')),
   PostLayout: dyn(() => import('./layouts/PostLayout')),
